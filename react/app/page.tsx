ba03d599e9a8f8f85b60a5e8a34cd2dc3c65e364
ekadhirapratama <eka.dhirapratama@gmail.com>
2024-09-09 17:11:08 +0700

    feat: copy layout from vue

--- react/app/page.tsx
+++ react/app/page.tsx
@@ -1,101 +1,49 @@
-import Image from "next/image";
+import AppNavbar from "@/components/AppNavbar";
+import AppHero from "@/components/AppHero";
+import Searchbar from "@/components/Searchbar";
+import ProductCard from "@/components/ProductCard";
+import { PRODUCTS } from '@/constants';
+import AppFooter from "@/components/AppFooter";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="bg-white">
+      <AppNavbar />
+      <AppHero />
+      <section className="mx-auto max-w-7xl py-24 min-h-[750px]">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-800 sm:text-6xl">Our Products</h1>
+        <p className="mt-6 text-lg leading-8 text-gray-800 max-w-3xl">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium veritatis consequatur
+          voluptatem tempora iusto? Error rerum, molestias reprehenderit veniam perspiciatis
+          obcaecati.
+        </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="grid grid-cols-4 gap-6 mt-8">
+          <div className="col-span-1 mb-4">
+            <Searchbar />
+          </div>
+          <div className="col-span-3" />
+          {PRODUCTS.map((product) => {
+            return (
+              <ProductCard
+                name={product.name}
+                description={product.description}
+                discount={product.discount}
+                originalPrice={product.originalPrice}
+              />
+            )
+          })}
+          <div className="col-span-4 flex justify-center mt-8">
+            <button
+              type="button"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Load more product
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+      <AppFooter />
+    </main>
   );
 }
---
+++ react/components/Searchbar.tsx
@@ -0,0 +1,14 @@
+export default function Searchbar() {
+  return (
+    <div className="mt-2">
+      <input
+        type="text"
+        name="search"
+        id="search"
+        className="block w-full rounded-lg border-0 py-3 px-3 text-xl text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        placeholder="find your product"
+        autoComplete="off"
+      />
+    </div>
+  );
+}
