45d938f3d6ea5fd811ec991ba88b46788e8da616
Agung Hidayat <agunghidayat10@gmail.com>
2024-09-13 10:58:33 +0700

    test(utils): create unit test toCamelCase

--- _javascript/src/utils/agung.test.ts
+++ _javascript/src/utils/agung.test.ts
@@ -1,9 +1,14 @@
 import { expect, it, describe } from 'vitest'
-import { getReadableFileSize } from './index'
-
+import { getReadableFileSize, toCamelCase } from './index'
 
 describe('[utils: getReadableFileSize]', () => {
     it('should return the size of the file with unit size', () => {
         expect(getReadableFileSize(2048)).toBe('2.0 KB')
+    })
+})
+
+describe('[utils: toCamelCase]', () => {
+    it('should return string with camelCased format', () => {
+        expect(toCamelCase('hello-warudo')).toBe('helloWarudo')
     })
 })
