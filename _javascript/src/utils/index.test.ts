07bff190be986fe381cb2491ee3315ea167f7f0e
Agung Hidayat <agunghidayat10@gmail.com>
2024-09-13 11:20:10 +0700

    refactor(utils): combine agung.test to utils/index

--- _javascript/src/utils/index.test.ts
+++ _javascript/src/utils/index.test.ts
@@ -1,5 +1,5 @@
 import { expect, it, describe } from 'vitest'
-import { double, formatCurrencyToIDR, calculatePriceWithDiscount } from './index'
+import { double, formatCurrencyToIDR, calculatePriceWithDiscount, getReadableFileSize, toCamelCase, capitalizeText, validateEmail } from './index'
 
 describe('[utils]: double', () => {
   it('should double a number', () => {
@@ -28,5 +28,33 @@ describe('[utils: calculatePriceWithDiscount]', () => {
 
   it('should throw an error if the discount is greater than 100', () => {
     expect(() => calculatePriceWithDiscount(1000, 150)).toThrow('Discount percentage must be between 0 and 100')
+  })
+})
+
+describe('[utils: getReadableFileSize]', () => {
+  it('should return the size of the file with unit size', () => {
+      expect(getReadableFileSize(1024)).toBe('1.0 KB')
+  })
+})
+
+describe('[utils: toCamelCase]', () => {
+  it('should return string with camelCased format', () => {
+      expect(toCamelCase('hello-warudo')).toBe('helloWarudo')
+  })
+})
+
+describe('[utils: capitalizeText]', () => {
+  it('should return string with capitalize every first char of word', () => {
+      expect(capitalizeText('Halo teman-teman frontend')).toBe('Halo Teman-teman Frontend')
+  })
+})
+
+describe('[utils: validateEmail]', () => {
+  it('should return "Valid Email"', () => {
+      expect(validateEmail('agung@example.com')).toBe('Valid email')
+  })
+
+  it('should return "Invalid Email"', () => {
+      expect(validateEmail('Waduh waduh waduh')).toBe('Invalid email')
   })
 })
